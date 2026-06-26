9d3bb74596235326c6c952f6a095c0c2ac44d430
zanee1205 <tdkhuee@gmail.com>
2026-06-26 10:39:31 +0700

    Update fixing failure can't predict the score

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,14 +1,16 @@
 import { Alert, Layout } from 'antd';
 import { Content } from 'antd/es/layout/layout';
+import { observer } from 'mobx-react-lite';
 import { Outlet } from 'react-router-dom';
 
+import { MatchDrawer } from '../../components/MatchDrawer/MatchDrawer.tsx';
 import { AppFooter } from '../footerSide.tsx';
 import { AppHeader } from '../headerSide.tsx';
 import styles from './MainLayout.module.scss';
 import type { TournamentMatch } from '../../../shared/types/tournamentMatch.ts';
 import { appStore } from '../../store/appStore.ts';
 
-export default function MainLayout({
+function MainLayout({
     refreshing,
 }: {
     dashboard?: NonNullable<typeof appStore.dashboard>;
@@ -23,6 +25,15 @@ export default function MainLayout({
                 <Outlet />
             </Content>
             <AppFooter />
+            <MatchDrawer
+                open={Boolean(appStore.selectedMatch)}
+                match={appStore.selectedMatch}
+                readOnly={Boolean(appStore.dashboard?.summary.locked)}
+                onClose={appStore.closeMatch}
+                onSavePrediction={appStore.savePrediction}
+            />
         </Layout>
     );
 }
+
+export default observer(MainLayout);
